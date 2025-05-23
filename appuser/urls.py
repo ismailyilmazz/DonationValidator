38ad55e4f06e64f8fc834e955b1cc2fc59dfd45d
Muhammet Emir AYDIN <memiraydin05@gmail.com>
2025-05-23 22:57:43 +0300

    Forgot my password page added

--- appuser/urls.py
+++ appuser/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import register_view,login_view,profile_view,logout_view,account_summary_view,stk_page_view
 from . import views
@@ -11,4 +12,8 @@ urlpatterns=[
     path('stk_page/', stk_page_view, name='stk_page'),
     path('account-summary/', account_summary_view, name='account-summary'),
     path('user/profile/', views.profile_view, name='profile'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- odev/settings.py
+++ odev/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'odev.wsgi.application'
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -88,6 +88,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.your-email-provider.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'memiraydin05@gmail.com'
+EMAIL_HOST_PASSWORD = 'Capgar123'
 
 
 # Password validation
