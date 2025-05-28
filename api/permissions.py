1e2ac03d4638111d7ea262ad62230b78807c918e
Abdullah ERGEN <theover29tr@gmail.com>
2025-05-28 18:57:31 +0300

    little

--- api/permissions.py
+++ api/permissions.py
@@ -1,10 +1,6 @@
 from rest_framework import permissions
 
 class IsNeedyOrReadOnly(permissions.BasePermission):
-    """
-    Sadece ilgili 'Need' kaydının sahibi (needy) PUT, PATCH ve DELETE yapabilir.
-    Diğer herkes sadece GET yapabilir.
-    """
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
