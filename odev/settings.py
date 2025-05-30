e861a67bc4a422469dc2952cafaf4b247714fa22
İsmail Yılmaz <92738540+ismailyilmazz@users.noreply.github.com>
2025-05-30 21:50:14 +0300

    Update settings.py

--- odev/settings.py
+++ odev/settings.py
@@ -23,7 +23,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zk^uldqqd%$=n3f*_h(wbtx9pb9vw8red@o)8pf0cfjoqs#lkp'
+SECRET_KEY = secret(secret)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
